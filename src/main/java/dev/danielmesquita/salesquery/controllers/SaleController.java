844d88038896d4b7367830eb9923f691fe01fcef
Daniel <danielmesquitadev@gmail.com>
2025-07-26 18:44:03 -0300

    feat: create summary controller method

--- src/main/java/dev/danielmesquita/salesquery/controllers/SaleController.java
+++ src/main/java/dev/danielmesquita/salesquery/controllers/SaleController.java
@@ -1,6 +1,7 @@
 package dev.danielmesquita.salesquery.controllers;
 
 import dev.danielmesquita.salesquery.dtos.SaleMinDTO;
+import dev.danielmesquita.salesquery.dtos.SaleSummaryDTO;
 import dev.danielmesquita.salesquery.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -24,6 +25,15 @@ public class SaleController {
       @RequestParam(name = "name", defaultValue = "") String name,
       Pageable pageable) {
     Page<SaleMinDTO> page = service.getReport(minDate, maxDate, name, pageable);
+    return ResponseEntity.ok(page);
+  }
+
+  @GetMapping(value = "/summary")
+  public ResponseEntity<Page<SaleSummaryDTO>> getSummary(
+      @RequestParam(name = "minDate", defaultValue = "") String minDate,
+      @RequestParam(name = "maxDate", defaultValue = "") String maxDate,
+      Pageable pageable) {
+    Page<SaleSummaryDTO> page = service.getSummary(minDate, maxDate, pageable);
     return ResponseEntity.ok(page);
   }
 }
