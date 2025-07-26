581be727f31eb2523d7764ef3883227a8388b350
Daniel <danielmesquitadev@gmail.com>
2025-07-26 18:24:51 -0300

    feat: create entities

---
+++ src/main/java/dev/danielmesquita/salesquery/SalesqueryApplication.java
@@ -0,0 +1,13 @@
+package dev.danielmesquita.salesquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesqueryApplication.class, args);
+	}
+
+}
