294abdf89b73e26962a8a0d54da1bc2a6dd21ffc
Daniel <danielmesquitadev@gmail.com>
2025-07-26 18:45:38 -0300

    refactor: change main class name

--- src/main/java/dev/danielmesquita/salesquery/SalesQueryApplication.java
+++ src/main/java/dev/danielmesquita/salesquery/SalesQueryApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SalesqueryApplication {
+public class SalesQueryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalesqueryApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SalesQueryApplication.class, args);
+  }
 }
