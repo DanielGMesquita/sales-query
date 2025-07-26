2380e2bec6d2f2fb4083d3039edd9ffb183ff2be
Daniel <danielmesquitadev@gmail.com>
2025-07-26 18:43:13 -0300

    feat: set summary logic

--- src/main/java/dev/danielmesquita/salesquery/services/SaleService.java
+++ src/main/java/dev/danielmesquita/salesquery/services/SaleService.java
@@ -1,15 +1,15 @@
 package dev.danielmesquita.salesquery.services;
 
 import dev.danielmesquita.salesquery.dtos.SaleMinDTO;
+import dev.danielmesquita.salesquery.dtos.SaleSummaryDTO;
 import dev.danielmesquita.salesquery.repositories.SaleRepository;
+import java.time.LocalDate;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.util.Map;
 
 @Service
 public class SaleService {
@@ -22,6 +22,12 @@ public class SaleService {
     Map<String, LocalDate> dateRange = parseDateRange(minDate, maxDate);
     return repository.findByDateBetweenAndName(
         dateRange.get("min"), dateRange.get("max"), pageable, name);
+  }
+
+  @Transactional(readOnly = true)
+  public Page<SaleSummaryDTO> getSummary(String minDate, String maxDate, Pageable pageable) {
+    Map<String, LocalDate> dateRange = parseDateRange(minDate, maxDate);
+    return repository.findByDateBetween(dateRange.get("min"), dateRange.get("max"), pageable);
   }
 
   private Map<String, LocalDate> parseDateRange(String minDate, String maxDate) {
